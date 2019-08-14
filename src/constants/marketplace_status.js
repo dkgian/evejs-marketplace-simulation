d3edd01168817b84a2ffacfeee2b6b9ad951a655
dkgian <duykhanhgian0510@gmail.com>
2019-08-14 11:52:53 +0200

    added common constants

---
+++ src/constants/marketplace_status.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AVAILABLE: 'AVAILABLE',
+  BUSY: 'BUSY',
+  OFFLINE: 'OFFLINE',
+}
---
+++ src/constants/task_status.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PENDING: 'PENDING',
+  PROCESSING: 'PROCESSING',
+  DONE: 'DONE',
+}

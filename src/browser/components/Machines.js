405bd3152c493865ea33ee9f09257ac7eb7c7ce6
dkgian <duykhanhgian0510@gmail.com>
2020-01-21 09:21:05 +0100

    added summary info for market

--- src/browser/components/Machines.js
+++ src/browser/components/Machines.js
@@ -1,47 +1,60 @@
 const Machines = function(props) {
   return (
     <React.Fragment>
-      <div id="machine1" className="col-4">
+      <div id="machine1" className="">
         <div className="card">
           <div className="card-header">
             <h5>Machine 1</h5>
           </div>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">Capabilities: 'grinding', 'coating'</li>
-            <li id="machine1status" className="list-group-item">Status: ... </li>
-            <li id="machine1balance" className="list-group-item">Balance: ... </li>
-            <li id="machine1wearOffLevel" className="list-group-item">Tool wear level: ... </li>
-            <li id="machine1toolingTimes" className="list-group-item">Tooling times: ... </li>
+            <li id="machine1status" className="list-group-item">Status: --- </li>
+            <li id="machine1balance" className="list-group-item">Balance: --- </li>
+            <li id="machine1wearOffLevel" className="list-group-item">Tool wear level: --- </li>
+            <li id="machine1toolingTimes" className="list-group-item">Tooling times: --- </li>
           </ul>
         </div>
       </div>
 
-      <div id="machine2" className="col-4">
+      <div id="machine2" className="">
         <div className="card">
           <div className="card-header">
             <h5>Machine 2</h5>
           </div>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">Capabilities: 'grinding', 'case-hardening'</li>
-            <li id="machine2status" className="list-group-item">Status: active</li>
-            <li id="machine2balance" className="list-group-item">Balance: 10</li>
-            <li id="machine2wearOffLevel" className="list-group-item">Tool wear level: 0</li>
-            <li id="machine2toolingTimes" className="list-group-item">Tooling times: 0</li>
+            <li id="machine2status" className="list-group-item">Status: ---</li>
+            <li id="machine2balance" className="list-group-item">Balance: ---</li>
+            <li id="machine2wearOffLevel" className="list-group-item">Tool wear level: ---</li>
+            <li id="machine2toolingTimes" className="list-group-item">Tooling times: ---</li>
           </ul>
         </div>
       </div>
 
-      <div id="machine3" className="col-4">
+      <div id="machine3" className="">
         <div className="card">
           <div className="card-header">
             <h5>Machine 3</h5>
           </div>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">Capabilities: 'coating', 'case-hardening'</li>
-            <li id="machine3status" className="list-group-item">Status: active</li>
-            <li id="machine3balance" className="list-group-item">Balance: 10</li>
-            <li id="machine3wearOffLevel" className="list-group-item">Tool wear level: 0</li>
-            <li id="machine3toolingTimes" className="list-group-item">Tooling times: 0</li>
+            <li id="machine3status" className="list-group-item">Status: ---</li>
+            <li id="machine3balance" className="list-group-item">Balance: ---</li>
+            <li id="machine3wearOffLevel" className="list-group-item">Tool wear level: ---</li>
+            <li id="machine3toolingTimes" className="list-group-item">Tooling times: ---</li>
+          </ul>
+        </div>
+      </div>
+
+      <div id="marketSummary" className="">
+        <div className="card">
+          <div className="card-header">
+            <h5>Summary</h5>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li id="marketBalance" className="list-group-item">Balance: ---</li>
+            <li id="marketToolingTimes" className="list-group-item">Tooling times: ---</li>
+            <li id="marketIdleTime" className="list-group-item">Idle time: ---</li>
           </ul>
         </div>
       </div>
